adaeb4a615b6f25c2e6a48453ea73af86fd669d2
RM <rmorris.code@gmail.com>
2022-07-13 21:27:30 -0700

    skeleton of download_size

--- data_getter/async_getter.py
+++ data_getter/async_getter.py
@@ -6,20 +6,35 @@ This file handles multithreading for data downloads from GBIF.
 
 References (for Rick):
 https://realpython.com/python-concurrency/
+https://codereview.stackexchange.com/questions/259112/async-download-of-files
+https://www.twilio.com/blog/working-with-files-asynchronously-in-python-using-aiofiles-and-asyncio
+
 '''
 
 import asyncio
 import httpx
 import os
 import urllib
+import aiofiles
 
 class AsyncGetter(object):
     def __int__(self, base_path: str='', sites: list=None):
         self.sites = sites
         self.base_path = base_path
 
-    async def download_site(self, session, url):
-        pass
+    async def download_site(self, client, url):
+        '''
+        Downloads a single binary file from the url..
+
+        :param client:
+        :param url:
+        :return:
+        '''
+        async with client.get(url) as response:
+            filename = self.make_filename(url=url)
+            async with aiofiles.open(filename, mode='w') as file:
+                async for data in response.content.iter_raw():
+                    await file.write(data)
 
     async def download_all_sites(self):
         client = httpx.Client()

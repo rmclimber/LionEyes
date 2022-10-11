fd6c3de2d54d645e4bc28b2b54d610ad12a27da7
RM <rmorris.code@gmail.com>
2022-10-10 20:55:43 -0700

    started working on data labeler: first up, set it up so that images can be randomly selected and moved to new folder

---
+++ data_cleaner/data_labeler.py
@@ -0,0 +1,30 @@
+'''
+Author: Rick Morris
+Repo: https://github.com/rmclimber/LionEyes
+
+This file randomly selects images for labeling by hand and saves them to a new
+directory.
+
+References (for Rick):
+
+'''
+
+import asyncio
+import sys
+
+import httpx
+import os
+import urllib
+import aiofiles
+import pandas as pd
+
+class DataLabeler():
+    seed = 42
+    num_samples = 500
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
